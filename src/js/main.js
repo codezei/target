c9ee68310131b547c8220216617395daf452fb8e
codezei <mwbox89@gmail.com>
2025-05-12 21:19:07 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+
+// function main() {
+//   scrollSmooth();
+// }
+
+// if (document.documentElement.clientWidth < 480) {
+//   window.addEventListener('scroll',
+//     function () {
+//       return setTimeout(main, 1000);
+//     }, {
+//       once: true
+//     });
+// } else {
+//   main();
+// };
